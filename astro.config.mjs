f1b79d0d810b2a8abc7920b5e8dea79bbf901cf9
mehtaz23 <mehtazmuntaqim@gmail.com>
2026-04-05 15:39:39 -0500

    initial astro nano template setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://mehtaz23.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
